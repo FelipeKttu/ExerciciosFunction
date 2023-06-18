4e6f740cc54d49d87a25cff04d2138a0ce9410e3
Felipe Kttu <kttudev@gmail.com>
2023-06-18 02:14:33 -0300

    Update #Exercicio2.py

--- #Exercicio2.py
+++ #Exercicio2.py
@@ -2,18 +2,20 @@
 '''Elaborar uma função (com retorno) que verifica se um número é positivo ou negativo.sendo
 que o valor de retorno será 1 se positivo, -1 se for negativo e 0 se for igual a 0'''
 
-N1 = float(input('Digite um número:'))
+num1 = float(input('Digite um número:'))
 
-def V(N):
-  if N1 > 0:
-    print('{} é positivo !!'.format(N1))
+def FunVer(N):
+  if num1 > 0:
+    print('{} é positivo !!'.format(num1))
     return 1
-  elif N1 < 0:
-    print('{} é negativo !!'.format(N1))
+  
+  elif num1 < 0:
+    print('{} é negativo !!'.format(num1))
     return -1
-  elif N1 == 0:
-    print('{} é neutro !!'.format(N1))
+  
+  elif num1 == 0:
+    print('{} é neutro !!'.format(num1))
     return 0
 
-resultado = V(N1)
+resultado = FunVer(num1)
 print(resultado)
