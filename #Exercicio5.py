23c2849605b39b8840629510cbf8fe89f48de0a8
Felipe Kttu <kttudev@gmail.com>
2023-06-18 02:20:16 -0300

    Update #Exercicio5.py

--- #Exercicio5.py
+++ #Exercicio5.py
@@ -1,14 +1,16 @@
 #Exercicio4
 '''Elaborar uma função (sem retorno) que ao receber um número deve converter para 
 fahrenheit e exibe o resultado na tela. A formula de conversão é
-F = (9 * C + 160) / 5'''
-C = float(input('Digite o valor para converter para Fahreinheit:'))
+F = (9 * celsius + 160) / 5'''
+
+cels = float(input('Digite o valor para converter para Fahreinheit:'))
 
 def F(graus):
-    VF = (C * 1.8)+ 32
-    print('{} Celsius convertidos para Fahrenheit é {}'.format(C,VF))
-    return C
+    VF = (graus * 1.8)+ 32
+    print('{} Celsius convertidos para Fahrenheit é {}'.format(graus, VF))
+    return cels
 
-resultado = F(C)
+resultado = F(cels)
 print(resultado)
+
 
