e60994f2766d1ce4874535486ed62c0786f530b5
Felipe Kttu <kttudev@gmail.com>
2023-06-18 02:21:57 -0300

    Update #Exercicio6.py

--- #Exercicio6.py
+++ #Exercicio6.py
@@ -1,14 +1,13 @@
-#Exercicio6
+#Exercicio7
 '''Elaborar uma Função (sem retorno) que ao receber um número deve converter para 
 Kelvin e exibe o resultado na tela. A Formula de conversão é
-K = C+273,15'''
+K = cels+273,15'''
 
-C = float(input('Digite o valor para converter para Fahreinheit:'))
+cels = float(input('Digite o valor para converter para Fahreinheit:'))
 
 def K(graus):
-    VK = C + 273.15
-    print('{} Celsius convertidos para Kelvin é {}'.format(C,VK))
-    return VK
+    valor_kelvin = cels + 273.15
+    print('{} Celsius convertidos para Kelvin é {}'.format(graus, valor_kelvin))
 
-resultado = K(C)
+resultado = K(cels)
 print(resultado)
